144505d23930de92723b6310ab92ec6daccc4c0c
faridborbar <faridbor93@gmail.com>
2015-10-08 02:43:54 -0300

    .
    elimine cosas innecesarias

--- codigoP2.py
+++ codigoP2.py
@@ -10,14 +10,13 @@ rho = 28
 w0 = [5, 5, 5]
 t0 = 0
 
-#creamos funcion adhoc para la usar el ode de scipy
 def Lorentz(t, w, s=sigma, r=rho, b=beta):
     X, Y, Z = w
     return [s*(Y-X), X*(r-Z)-Y, X*Y-b*Z]
 
-#seteamos el la funcion ode para resolver el sistema de EDOS
+
 r = ode(Lorentz)
-r.set_integrator('dopri5') #comando para usar RK4
+r.set_integrator('dopri5')
 r.set_initial_value(w0, t0)
 
 t = np.linspace(t0, 50, 10000)
@@ -26,11 +25,9 @@ X = np.zeros(len(t))
 Y = np.zeros(len(t))
 Z = np.zeros(len(t))
 
-#iteramos usando la funcion ode antes mencionada
 for i in range(len(t)):
     r.integrate(t[i])
     X[i], Y[i], Z[i] = r.y
-
 
 fig = plt.figure()
 figura = fig.add_subplot(111, projection='3d')
